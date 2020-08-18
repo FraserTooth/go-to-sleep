94ead28da6ea4e8c0b16c7ea109a473bcc91836f
Fraser Tooth <frasertooth@gmail.com>
2020-08-18 23:28:04 +0900

    Deal with out of bounds later

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@ async function run() {
         const timefacts = {
           time,
           timezoneString,
-          outOfBounds,
+          // outOfBounds,
         };
 
         console.log(`Its currently ${timeInLocation} where you are boyyy`);
