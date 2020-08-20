be45259b17dc448ec2381581dc846b94c94fa70a
Fraser Tooth <frasertooth@gmail.com>
2020-08-20 23:56:03 +0900

    Added PRs

--- dist/index.js
+++ dist/index.js
@@ -855,10 +855,19 @@ const github = __webpack_require__(858);
 const { Octokit } = __webpack_require__(998);
 const fetch = __webpack_require__(219);
 
-function convertGithubTIme(timezoneString, itemID, item, context, octokit) {
-  [tzHours, tzMinutes] = timezoneString.split(":").map(Number);
+function convertGithubTIme(
+  timestamp,
+  timezoneString = null,
+  custom_timezone = null
+) {
+  let tzHours, tzMinutes;
+  if (custom_timezone) {
+    [tzHours, tzMinutes] = custom_timezone.split(":").map(Number);
+  } else {
+    [tzHours, tzMinutes] = timezoneString.split(":").map(Number);
+  }
 
-  const time = new Date(item.timestamp);
+  const time = new Date(timestamp);
   const hour = time.getHours();
   const mins = time.getMinutes();
 
@@ -870,57 +879,34 @@ function convertGithubTIme(timezoneString, itemID, item, context, octokit) {
 
   const outOfBounds = timeInLocation < 9 || 19 < timeInLocation;
 
-  const timefacts = {
+  return {
     time,
-    timezoneString,
+    timeInLocation,
     outOfBounds,
   };
-
-  console.log(`Its currently ${timeInLocation} where you are.`);
-
-  if (outOfBounds) {
-    console.log(
-      `You are very naughty working outside of work hours, get some rest!`
-    );
-
-    const repository = context.payload.repository;
-
-    octokit.repos.createCommitComment({
-      owner: repository.owner.login,
-      repo: repository.name,
-      commit_sha: itemID,
-      body: "poo poo",
-    });
-  }
-
-  return timefacts;
 }
 
 async function run() {
   try {
     const githubToken = core.getInput("GITHUB_TOKEN");
-    const customMessage = core.getInput("custom_message");
     const { context } = github;
     const event = context.eventName;
+    const repository = context.payload.repository;
 
     const octokit = new Octokit({ auth: githubToken });
 
-    const senderObject = context.payload.sender;
+    const message =
+      core.getInput("custom_message") ||
+      "Its quite late, maybe you should go to sleep!";
 
-    const senderAPIURL = senderObject.url;
-    const userDataResponse = await fetch(senderAPIURL);
-    const userData = await userDataResponse.json();
-    const userLocation = userData.location;
-
-    console.log(`Event: ${event}`);
+    console.log(`Event Type: ${event}`);
     // console.log(context.payload);
     // console.log("Context");
     // console.log(context);
     // console.log("Payload");
     // console.log(JSON.stringify(context.payload, undefined, 2));
 
-    let timestamp = new Date();
-    let timezone;
+    let custom_timezone = core.getInput("timezone");
     const timezoneRegex = /[\+\-]\d\d:\d\d/gm;
 
     /* Event Types
@@ -932,78 +918,61 @@ async function run() {
     //   issue_comment - comment on issue OR pr itself
     */
 
+    // A Push Event is the Only Event where we can easily find out the person's timezone from the data
     if (event === "push" && context.payload.commits) {
-      timestamp = context.payload.commits.map((commit) => {
+      context.payload.commits.forEach((commit) => {
         const timezoneString = timezoneRegex.exec(commit.timestamp)[0];
 
-        return convertGithubTIme(
-          timezoneString,
-          commit.id,
-          commit,
-          context,
-          octokit
-        );
+        ({ time, timeInLocation, outOfBounds } = convertGithubTIme(
+          commit.timestamp,
+          timezoneString
+        ));
+
+        console.log(`Its currently ${timeInLocation} where you are.`);
+
+        if (outOfBounds) {
+          octokit.repos.createCommitComment({
+            owner: repository.owner.login,
+            repo: repository.name,
+            commit_sha: commit.id,
+            body: message,
+          });
+        }
       });
+      return; // Finish
     }
 
-    // if (context.payload.review) {
-    //   if (context.payload.action === "submitted") {
-    //     const issueNumber = context.payload.pull_request.number;
-    //     const repository = context.payload.repository;
+    // Need Custom Timestamp to proceed
+    if (!custom_timezone) return;
 
-    //     const reviewObject = context.payload.review;
+    if (event === "pull_request" && context.payload.pull_request) {
+      ({ time, timeInLocation, outOfBounds } = convertGithubTIme(
+        commit.timestamp,
+        null,
+        custom_timezone
+      ));
 
-    //     const commentAuthor = reviewObject.user.login;
+      console.log(`Its currently ${timeInLocation} where you are.`);
 
-    //     const message = customMessage
-    //       ? customMessage
-    //       : `<img src="https://media.giphy.com/media/3ohzdQ1IynzclJldUQ/giphy.gif" width="400"/> </br>
-    //                                   Hey @${commentAuthor}! 👋 <br/> PRs and issues should be safe environments but your comment: <strong>"${toxicComment}"</strong> was classified as potentially toxic! 😔</br>
-    //                                   Please consider spending a few seconds editing it and feel free to delete me afterwards! 🙂`;
-
-    //     return octokit.issues.createComment({
-    //       owner: repository.owner.login,
-    //       repo: repository.name,
-    //       issue_number: issueNumber,
-    //       body: message,
-    //     });
-    //   }
-    // }
-    // if (context.payload.comment) {
-    //   if (
-    //     context.payload.action === "created" ||
-    //     context.payoad.action === "edited"
-    //   ) {
-    //     const issueNumber = context.payload.issue.number;
-    //     const repository = context.payload.repository;
-    //     const octokit = new github.GitHub(githubToken);
-
-    //     const comments = [];
-    //     const commentsObjects = [];
-
-    //     const latestComment = context.payload.comment;
-    //     comments.push(latestComment.body);
-    //     commentsObjects.push(latestComment);
-
-    //     const commentAuthor = commentsObjects[index].user.login;
-    //     toxicComment = commentsObjects[index].body;
-    //     const message = customMessage
-    //       ? customMessage
-    //       : `<img src="https://media.giphy.com/media/3ohzdQ1IynzclJldUQ/giphy.gif" width="400"/> </br>
-    //                                   Hey @${commentAuthor}! 👋 <br/> PRs and issues should be safe environments but your comment: <strong>"${toxicComment}"</strong> was classified as potentially toxic! 😔</br>
-    //                                   Please consider spending a few seconds editing it and feel free to delete me afterwards! 🙂`;
-
-    //     return octokit.issues.createComment({
-    //       owner: repository.owner.login,
-    //       repo: repository.name,
-    //       issue_number: issueNumber,
-    //       body: message,
-    //     });
-    //   }
-    // }
+      if (outOfBounds) {
+        octokit.issues.createComment({
+          owner: repository.owner.login,
+          repo: repository.name,
+          issue_number: context.payload.pull_request.number,
+          body: message,
+        });
+      }
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
+
+  const senderObject = context.payload.sender;
+
+  const senderAPIURL = senderObject.url;
+  const userDataResponse = await fetch(senderAPIURL);
+  const userData = await userDataResponse.json();
+  const userLocation = userData.location;
 }
 
 run();
--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ const github = require("@actions/github");
 const { Octokit } = require("@octokit/rest");
 const fetch = require("node-fetch");
 
-function convertGithubTIme(timezoneString, timestamp) {
-  [tzHours, tzMinutes] = timezoneString.split(":").map(Number);
+function convertGithubTIme(
+  timestamp,
+  timezoneString = null,
+  custom_timezone = null
+) {
+  let tzHours, tzMinutes;
+  if (custom_timezone) {
+    [tzHours, tzMinutes] = custom_timezone.split(":").map(Number);
+  } else {
+    [tzHours, tzMinutes] = timezoneString.split(":").map(Number);
+  }
 
   const time = new Date(timestamp);
   const hour = time.getHours();
@@ -28,31 +37,24 @@ function convertGithubTIme(timezoneString, timestamp) {
 async function run() {
   try {
     const githubToken = core.getInput("GITHUB_TOKEN");
-    const customMessage = core.getInput("custom_message");
     const { context } = github;
     const event = context.eventName;
     const repository = context.payload.repository;
 
     const octokit = new Octokit({ auth: githubToken });
 
-    const senderObject = context.payload.sender;
+    const message =
+      core.getInput("custom_message") ||
+      "Its quite late, maybe you should go to sleep!";
 
-    const senderAPIURL = senderObject.url;
-    const userDataResponse = await fetch(senderAPIURL);
-    const userData = await userDataResponse.json();
-    const userLocation = userData.location;
-
-    const message = "Its quite late, maybe you should go to sleep!";
-
-    console.log(`Event: ${event}`);
+    console.log(`Event Type: ${event}`);
     // console.log(context.payload);
     // console.log("Context");
     // console.log(context);
     // console.log("Payload");
     // console.log(JSON.stringify(context.payload, undefined, 2));
 
-    let timestamp = new Date();
-    let timezone;
+    let custom_timezone = core.getInput("timezone");
     const timezoneRegex = /[\+\-]\d\d:\d\d/gm;
 
     /* Event Types
@@ -64,13 +66,14 @@ async function run() {
     //   issue_comment - comment on issue OR pr itself
     */
 
+    // A Push Event is the Only Event where we can easily find out the person's timezone from the data
     if (event === "push" && context.payload.commits) {
       context.payload.commits.forEach((commit) => {
         const timezoneString = timezoneRegex.exec(commit.timestamp)[0];
 
         ({ time, timeInLocation, outOfBounds } = convertGithubTIme(
-          timezoneString,
-          commit.timestamp
+          commit.timestamp,
+          timezoneString
         ));
 
         console.log(`Its currently ${timeInLocation} where you are.`);
@@ -79,71 +82,45 @@ async function run() {
           octokit.repos.createCommitComment({
             owner: repository.owner.login,
             repo: repository.name,
-            commit_sha: itemID,
+            commit_sha: commit.id,
             body: message,
           });
         }
       });
+      return; // Finish
     }
 
-    // if (context.payload.review) {
-    //   if (context.payload.action === "submitted") {
-    //     const issueNumber = context.payload.pull_request.number;
-    //     const repository = context.payload.repository;
+    // Need Custom Timestamp to proceed
+    if (!custom_timezone) return;
 
-    //     const reviewObject = context.payload.review;
+    if (event === "pull_request" && context.payload.pull_request) {
+      ({ time, timeInLocation, outOfBounds } = convertGithubTIme(
+        commit.timestamp,
+        null,
+        custom_timezone
+      ));
 
-    //     const commentAuthor = reviewObject.user.login;
+      console.log(`Its currently ${timeInLocation} where you are.`);
 
-    //     const message = customMessage
-    //       ? customMessage
-    //       : `<img src="https://media.giphy.com/media/3ohzdQ1IynzclJldUQ/giphy.gif" width="400"/> </br>
-    //                                   Hey @${commentAuthor}! 👋 <br/> PRs and issues should be safe environments but your comment: <strong>"${toxicComment}"</strong> was classified as potentially toxic! 😔</br>
-    //                                   Please consider spending a few seconds editing it and feel free to delete me afterwards! 🙂`;
-
-    //     return octokit.issues.createComment({
-    //       owner: repository.owner.login,
-    //       repo: repository.name,
-    //       issue_number: issueNumber,
-    //       body: message,
-    //     });
-    //   }
-    // }
-    // if (context.payload.comment) {
-    //   if (
-    //     context.payload.action === "created" ||
-    //     context.payoad.action === "edited"
-    //   ) {
-    //     const issueNumber = context.payload.issue.number;
-    //     const repository = context.payload.repository;
-    //     const octokit = new github.GitHub(githubToken);
-
-    //     const comments = [];
-    //     const commentsObjects = [];
-
-    //     const latestComment = context.payload.comment;
-    //     comments.push(latestComment.body);
-    //     commentsObjects.push(latestComment);
-
-    //     const commentAuthor = commentsObjects[index].user.login;
-    //     toxicComment = commentsObjects[index].body;
-    //     const message = customMessage
-    //       ? customMessage
-    //       : `<img src="https://media.giphy.com/media/3ohzdQ1IynzclJldUQ/giphy.gif" width="400"/> </br>
-    //                                   Hey @${commentAuthor}! 👋 <br/> PRs and issues should be safe environments but your comment: <strong>"${toxicComment}"</strong> was classified as potentially toxic! 😔</br>
-    //                                   Please consider spending a few seconds editing it and feel free to delete me afterwards! 🙂`;
-
-    //     return octokit.issues.createComment({
-    //       owner: repository.owner.login,
-    //       repo: repository.name,
-    //       issue_number: issueNumber,
-    //       body: message,
-    //     });
-    //   }
-    // }
+      if (outOfBounds) {
+        octokit.issues.createComment({
+          owner: repository.owner.login,
+          repo: repository.name,
+          issue_number: context.payload.pull_request.number,
+          body: message,
+        });
+      }
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
+
+  const senderObject = context.payload.sender;
+
+  const senderAPIURL = senderObject.url;
+  const userDataResponse = await fetch(senderAPIURL);
+  const userData = await userDataResponse.json();
+  const userLocation = userData.location;
 }
 
 run();
